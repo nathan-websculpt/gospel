89cd874aa081cf3d00845f7851fabacc4445c5dc
nathan-websculpt <websculpt.nathan@gmail.com>
2024-05-21 11:11:53 -0500

    base url for metadata will not work as-is; would require vercel auth

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -9,9 +9,11 @@ export const getMetadata = ({
   description: string;
   imageRelativePath?: string;
 }): Metadata => {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : `http://localhost:${process.env.PORT || 3000}`;
+  // const baseUrl = process.env.VERCEL_URL
+  //   ? `https://${process.env.VERCEL_URL}`
+  //   : `http://localhost:${process.env.PORT || 3000}`;
+  // you would have to be authenticated in vercel to see this, won't work for socials
+  const baseUrl = "https://www.gospelonchain.com";
   const imageUrl = `${baseUrl}${imageRelativePath}`;
   return {
     title: title,
