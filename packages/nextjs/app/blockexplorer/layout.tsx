c5a471f1d85ef812a5e6a35382f33646a905aff3
nathan-websculpt <websculpt.nathan@gmail.com>
2024-07-05 10:00:47 -0500

    metadata changes, robots to noindex on some dev pages

--- packages/nextjs/app/blockexplorer/layout.tsx
+++ packages/nextjs/app/blockexplorer/layout.tsx
@@ -3,6 +3,7 @@ import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 export const metadata = getMetadata({
   title: "Block Explorer",
   description: "Block Explorer created with 🏗 Scaffold-ETH 2",
+  robots: { index: false, follow: false },
 });
 
 const BlockExplorerLayout = ({ children }: { children: React.ReactNode }) => {
