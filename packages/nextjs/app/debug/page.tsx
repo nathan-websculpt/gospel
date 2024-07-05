de0ebc250ca9478172bfaff4c12e9b9f42e60e26
nathan-websculpt <websculpt.nathan@gmail.com>
2024-07-05 11:39:58 -0500

    metadata fix on debug page

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -1,18 +1,12 @@
 import { DebugContracts } from "./_components/DebugContracts";
 import type { NextPage } from "next";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
-
-export const metadata = getMetadata({
-  title: "Debug Contracts",
-  description: "Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way",
-});
 
 const Debug: NextPage = () => {
   return (
     <>
       <DebugContracts />
-      <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Debug Contracts</h1>
+      <div className="p-10 mt-8 text-center bg-secondary">
+        <h1 className="my-0 text-4xl">Debug Contracts</h1>
         <p className="text-neutral">
           You can debug & interact with your deployed contracts here.
           <br /> Check{" "}
