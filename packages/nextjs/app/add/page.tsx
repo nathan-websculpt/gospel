3135bce9718706c61babd5d0d7bacb01e0b3c237
nathan-websculpt <websculpt.nathan@gmail.com>
2024-07-05 12:56:17 -0500

    small changes to copy; changes to footer on mobile

--- packages/nextjs/app/add/page.tsx
+++ packages/nextjs/app/add/page.tsx
@@ -9,11 +9,11 @@ const AddVersesPage: NextPage = () => {
     <>
       <div className="w-full px-6 pt-10 pb-8 shadow-xl sm:my-auto bg-secondary sm:mx-auto md:w-5/6 sm:rounded-lg sm:px-10">
         <div className="flex flex-col items-center flex-grow w-full pt-10 mx-auto md:w-2/3">
-          <div className="px-5">
+          <div className="px-5 prose lg:prose-lg">
             <h1 className="text-center">
-              <span className="block mb-2 text-4xl">Add Batch Verses</span>
-              <span className="block text-2xl font-bold">
-                add some copy here .... dev only ... try to add a verse, you can't.
+              <span className="block mb-2 text-2xl">Add Batch Verses</span>
+              <span className="block text-xl font-bold">
+                Only websculpt.eth can add verses.
               </span>
             </h1>
           </div>
