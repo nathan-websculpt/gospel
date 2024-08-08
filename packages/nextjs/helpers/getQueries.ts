21f46e779ad69d738110c43f68d45fc3a9578909
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-08 10:00:10 -0500

    GQL_VERSES_For_Display not needed on PROD

--- packages/nextjs/helpers/getQueries.ts
+++ packages/nextjs/helpers/getQueries.ts
@@ -1,20 +1,5 @@
 import { gql } from "@apollo/client";
 
-// for the READ page (while dev is adding book)
-export const GQL_VERSES_For_Display = () => {
-  return gql`
-    query ($limit: Int!, $offset: Int!) {
-      verses(orderBy: verseId, orderDirection: asc, first: $limit, skip: $offset) {
-        id
-        verseId
-        chapterNumber
-        verseNumber
-        verseContent
-      }
-    }
-  `;
-};
-
 // for the READ page (after done uploading book)
 // initial query on page load
 // and for Chapter DDL
