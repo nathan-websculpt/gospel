ef59bd732a0d77133f181933e28326b27e83dbaa
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-08 11:27:13 -0500

    testing i18n lang

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
