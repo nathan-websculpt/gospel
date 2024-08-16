a7f8423185b6e50d8d745eeb6f98b979067761a1
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-16 12:18:16 -0500

    New Book DDL refactored to own component

---
+++ packages/nextjs/components/helpers/BookDDL.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GQL_BOOKS_List } from "~~/helpers/getQueries";
+
+interface BookDDLProps {
+  selectedContract: string;
+  setSelectedContract?: Dispatch<SetStateAction<string | null>>;
+  setSelectedBookId: Dispatch<SetStateAction<string>>;
+  changeContract: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export const BookDDL = ({ selectedContract, setSelectedContract, setSelectedBookId, changeContract }: BookDDLProps) => {
+  const { loading, error, data } = useQuery(GQL_BOOKS_List());
+
+  useEffect(() => {
+    if (error !== undefined && error !== null) console.log("GQL_BOOKS_List Query Error: ", error);
+  }, [error]);
+
+  useEffect(() => {
+    if (data !== undefined && data !== null) {
+      setSelectedContract(data?.books[0].title);
+      setSelectedBookId(data?.books[0].id);
+      console.log("GQL_BOOKS_List Query DATA: ", data);
+    }
+  }, [data]);
+
+  return (
+    <>
+      {!loading && (
+        <select
+          className="w-32 px-2 py-2 mr-1 text-xs rounded-none sm:px-6 sm:py-2 sm:mr-2 sm:text-sm md:text-md lg:text-lg sm:w-44 btn btn-primary"
+          value={selectedContract}
+          onChange={changeContract}
+          aria-label="Change Book"
+        >
+          {data?.books.map(b => (
+            <option key={b.id} data-bookid={b.id} value={b.title}>
+              {b.title}
+            </option>
+          ))}
+        </select>
+      )}
+    </>
+  );
+};
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -603,7 +603,7 @@ const deployedContracts = {
   },
   31337: {
     John: {
-      address: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+      address: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
       abi: [
         {
           inputs: [
@@ -912,7 +912,7 @@ const deployedContracts = {
       },
     },
     Mark: {
-      address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+      address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
       abi: [
         {
           inputs: [
