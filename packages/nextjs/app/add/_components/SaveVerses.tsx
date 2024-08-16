882809ed6cee4fc9355df576163ccfe2c6a4a16f
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-16 12:18:16 -0500

    New Book DDL refactored to own component

--- packages/nextjs/app/add/_components/SaveVerses.tsx
+++ packages/nextjs/app/add/_components/SaveVerses.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { useQuery } from "@apollo/client";
-import { GQL_BOOKS_List } from "~~/helpers/getQueries";
+import { BookDDL } from "~~/components/helpers/BookDDL";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 interface VerseProps {
@@ -11,13 +10,11 @@ interface VerseProps {
 }
 
 export const SaveVerses = (_v: VerseProps) => {
-  const [selectedContract, setSelectedContract] = useState("");
-  const [selectedBookId, setSelectedBookId] = useState("");
+  const [selectedContract, setSelectedContract] = useState<string>("");
+  const [selectedBookId, setSelectedBookId] = useState<string>("");
 
   const { writeContractAsync: writeToJohn } = useScaffoldWriteContract("John");
   const { writeContractAsync: writeToMark } = useScaffoldWriteContract("Mark");
-
-  const { loading, error, data } = useQuery(GQL_BOOKS_List());
 
   useEffect(() => {
     console.log("Selected contract changed to:", selectedContract);
@@ -26,18 +23,6 @@ export const SaveVerses = (_v: VerseProps) => {
   useEffect(() => {
     console.log("Selected book ID changed to:", selectedBookId);
   }, [selectedBookId]);
-
-  useEffect(() => {
-    if (error !== undefined && error !== null) console.log("GQL_BOOKS_List Query Error: ", error);
-  }, [error]);
-
-  useEffect(() => {
-    if (data !== undefined && data !== null) { 
-      setSelectedContract(data?.books[0].title);
-      setSelectedBookId(data?.books[0].id);
-      console.log("GQL_BOOKS_List Query DATA: ", data);
-    }
-  }, [data]);
 
   const writeAsync = async () => {
     try {
@@ -57,27 +42,19 @@ export const SaveVerses = (_v: VerseProps) => {
     }
   };
 
-  const changeContract = (e) => {
+  const changeContract = e => {
     setSelectedContract(e.target.value);
     setSelectedBookId(e.target.selectedOptions[0].getAttribute("data-bookid"));
   };
 
   return (
     <>
-      {!loading && (
-        <select
-          className="w-32 px-2 py-2 mr-1 text-xs rounded-none sm:px-6 sm:py-2 sm:mr-2 sm:text-sm md:text-md lg:text-lg sm:w-44 btn btn-primary"
-          value={selectedContract}
-          onChange={changeContract}
-          aria-label="Change Book"
-        >
-          {data?.books.map(b => (
-            <option key={b.id} data-bookid={b.id} value={b.title}>
-              {b.title}
-            </option>
-          ))}
-        </select>
-      )}
+      <BookDDL
+        selectedContract={selectedContract}
+        setSelectedContract={setSelectedContract}
+        setSelectedBookId={setSelectedBookId}
+        changeContract={changeContract}
+      />
 
       <button className="btn btn-primary" onClick={() => writeAsync()}>
         BATCH SAVE ONCHAIN
