7f8966125acc05f3d5b9153d5f6e0f67d1f700d5
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-18 09:31:22 -0500

    addVerses moved book ddl up one component

--- packages/nextjs/app/add/_components/SaveVerses.tsx
+++ packages/nextjs/app/add/_components/SaveVerses.tsx
@@ -1,38 +1,37 @@
 import { useEffect, useState } from "react";
-import { BookDDL } from "~~/components/helpers/BookDDL";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 interface VerseProps {
-  bookId: string;
   content: string[];
   chapterNum: bigint[];
   verseNum: bigint[];
+  selectedContract: string;
+  setSelectedContract: Dispatch<SetStateAction<string | null>>;
+  selectedBookId: string;
+  setSelectedBookId: Dispatch<SetStateAction<string>>;
 }
 
 export const SaveVerses = (_v: VerseProps) => {
-  const [selectedContract, setSelectedContract] = useState<string>("");
-  const [selectedBookId, setSelectedBookId] = useState<string>("");
-
   const { writeContractAsync: writeToJohn } = useScaffoldWriteContract("John");
   const { writeContractAsync: writeToMark } = useScaffoldWriteContract("Mark");
 
   useEffect(() => {
-    console.log("Selected contract changed to:", selectedContract);
-  }, [selectedContract]);
+    console.log("Selected contract changed to:", _v.selectedContract);
+  }, [_v.selectedContract]);
 
   useEffect(() => {
-    console.log("Selected book ID changed to:", selectedBookId);
-  }, [selectedBookId]);
+    console.log("Selected book ID changed to:", _v.selectedBookId);
+  }, [_v.selectedBookId]);
 
   const writeAsync = async () => {
     try {
-      const args = [selectedBookId, _v?.verseNum, _v?.chapterNum, _v?.content];
+      const args = [_v.selectedBookId, _v?.verseNum, _v?.chapterNum, _v?.content];
       const contractCall = {
         functionName: "addBatchVerses",
         args: args,
       };
 
-      if (selectedContract === "Mark") {
+      if (_v.selectedContract === "Mark") {
         await writeToMark(contractCall);
       } else {
         await writeToJohn(contractCall);
@@ -44,12 +43,6 @@ export const SaveVerses = (_v: VerseProps) => {
 
   return (
     <>
-      <BookDDL
-        selectedContract={selectedContract}
-        setSelectedContract={setSelectedContract}
-        setSelectedBookId={setSelectedBookId}
-      />
-
       <button className="btn btn-primary" onClick={() => writeAsync()}>
         BATCH SAVE ONCHAIN
       </button>
