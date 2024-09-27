c2b6a18f79cf9be2c242fff3bf584537aeec94d0
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-27 14:22:31 -0500

    refactoring clone deploys in front-end; created a warning/bug but logic does work

--- packages/nextjs/components/helpers/BookContractDDL.tsx
+++ packages/nextjs/components/helpers/BookContractDDL.tsx
@@ -1,28 +1,27 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Address } from "../scaffold-eth";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
 interface BookContractDDLProps {
-  contractNames: ContractName[];
-  clonedContractsAddresses: string[];
+  listOfBookContracts: any[];
   setSelectedContract: Dispatch<String>;
 }
 
-export const BookContractDDL = ({
-  contractNames,
-  clonedContractsAddresses,
-  setSelectedContract,
-}: BookContractDDLProps) => {
+export const BookContractDDL = ({ listOfBookContracts, setSelectedContract }: BookContractDDLProps) => {
   const dropdownRef = useRef<HTMLDetailsElement>(null);
   const closeDropdown = () => {
     dropdownRef.current?.removeAttribute("open");
   };
   useOutsideClick(dropdownRef, closeDropdown);
 
+  useEffect(() => {
+    console.log("listOfBookContracts: ", listOfBookContracts);
+  }, [listOfBookContracts]);
+
   return (
     <>
-      {contractNames?.length > 1 && (
+      {listOfBookContracts?.length > 1 && (
         <div className="flex flex-row flex-wrap w-full gap-2 px-6 pb-1 max-w-7xl lg:px-10">
           <details ref={dropdownRef} className="leading-3 dropdown dropdown-right">
             <summary tabIndex={0} className="btn btn-secondary btn-sm shadow-md dropdown-toggle gap-0 !h-auto">
@@ -30,16 +29,18 @@ export const BookContractDDL = ({
               <ChevronDownIcon className="w-4 h-6 ml-2 sm:ml-0" />
             </summary>
             <ul className="dropdown-content menu z-[100] p-2 mt-2 shadow-center shadow-accent bg-base-200 rounded-box gap-1">
-              {clonedContractsAddresses?.map(address => (
+              {listOfBookContracts?.map(b => (
                 <li
-                  key={address}
+                  key={b.bAddr}
                   onClick={() => {
-                    setSelectedContract(address);
+                    setSelectedContract(b.bAddr);
                     closeDropdown();
                   }}
-                  onKeyUp={() => setSelectedContract(address)}
+                  onKeyUp={() => setSelectedContract(b.bAddr)}
                 >
-                  <Address address={address} disableAddressLink={true} />
+                  <span>
+                    {b.title} <Address address={b.bAddr} disableAddressLink={true} size="sm" format="short" />
+                  </span>
                 </li>
               ))}
             </ul>
