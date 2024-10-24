1d372a09cdacaafd2b80f951b6e6e83deca3fbf5
nathan-websculpt <websculpt.nathan@gmail.com>
2024-10-24 07:57:09 -0500

    prepping for release

--- packages/hardhat/contracts/BookDeployer.sol
+++ packages/hardhat/contracts/BookDeployer.sol
@@ -4,8 +4,37 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./BookManager.sol";
 
-
-
+ /*
+ * @title BookDeployer
+ * @author Nathan - 0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350
+ * @notice BookDeployer will allow for multiple books (BookManager contracts) to be deployed (via deployBook function)
+ * @dev this code is considered to be at version 0.0.2; note that 0.0.1 is at Optimism address: 0x29BB1313321dbA27Ad074DD6AD2943040319B439
+ * 0.0.1 was a single book (Gospel of John) stored onto Optimism; however, 0.0.2 will allow multiple books to be bound together.
+ *
+ * The intention of these smart contracts is to store/confirm (verse-by-verse) Biblical books (KJV) onto the blockchain.
+ * I strongly suspect that I will upload the Gospels + Acts, gather more data from this process, and then begin on another version of these contracts.
+ * Since I can not come back to update this later, I will say that as of the time of writing this,
+ * I think it is important to protect the Gospels and the history of the early Church BEFORE the American election.
+ * This is more important than waiting another 3 months for a "perfect" smart contract to be written (by the one person working on this).
+ *
+ * If you are reading this from a future time: The year is 2024, and my world is in a fallen state.
+ * This is not just a project that some developer used for a portfolio ... it is a move against a type of tyranny very few people from my time saw. 
+ *
+ * Do I want to store the whole Bible? Yes (and the Greek/Hebrew, and multiple versions, etc). 
+ * I'm broke. I was fired after releasing the first Gospel.
+ * I have, however, received $300 in donations. I am going to use that to store the Gospels + Acts. It should be enough (and allow for mistakes to occur). 
+ * My current thinking is that – before going for all the books and their versions – this code will go through at least 1 more update.
+ * Meanwhile, I am also thinking that we need a whole new type of "blockchain" just for text.
+ * 
+ * Pray that the world and the word remain together. Pray that the season be little.
+ * 
+ *      from:
+ *      Nathan
+ *      https://github.com/nathan-websculpt
+ *      0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350
+ *      October 24th, 2024 – 7:37 AM
+ */
+ 
 contract BookDeployer is Ownable {
 	struct Deployment {
 		address bookAddress;
