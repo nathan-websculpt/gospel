c13777b34a7efbfa4fdfbe1ef898bd54de0a28db
nathan-websculpt <websculpt.nathan@gmail.com>
2024-10-24 10:07:46 -0500

    deployed to optimism

--- packages/hardhat/deploy/00_deploy_book_deployer.ts
+++ packages/hardhat/deploy/00_deploy_book_deployer.ts
@@ -21,8 +21,8 @@ const deployBookDeployer: DeployFunction = async function (hre: HardhatRuntimeEn
   */
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
-  //const owner = "0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350"; //PROD WALLET
-  const owner = "0xf0ADAE0823444d70Eb5826F3C26b3704611c759A"; //todo: PRODTODO:: change to fe wallet
+  const owner = "0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350"; //PROD WALLET
+  // const owner = "0xf0ADAE0823444d70Eb5826F3C26b3704611c759A"; //todo: PRODTODO:: change to fe wallet
   
   console.log("\n");
 
