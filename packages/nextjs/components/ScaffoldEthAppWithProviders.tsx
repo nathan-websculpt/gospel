863a4baa1b3dd6dcf40dfe5b6623bcf9897f048f
nathan-websculpt <websculpt.nathan@gmail.com>
2025-04-29 17:25:13 -0500

    trying to get around Vercel issues

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useTheme } from "next-themes";
@@ -56,19 +56,12 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
 
   const [mounted, setMounted] = useState(false);
 
-  const subgraphUri = "https://api.studio.thegraph.com/query/60402/gospel-v2/version/latest"; //PRODTODO
-  // const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract"; //PRODTODO
-  const apolloClient = new ApolloClient({
-    uri: subgraphUri,
-    cache: new InMemoryCache(),
-  });
-
   useEffect(() => {
     setMounted(true);
   }, []);
 
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={createApolloClient()}>
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
           <ProgressBar />
---
+++ packages/nextjs/lib/apollo-client.ts
@@ -0,0 +1,30 @@
+// lib/apollo-client.js
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+  const subgraphUri = "https://gateway.thegraph.com/api/subgraphs/id/FUv8Kpg9nkGhwtALr4YCYJwLDuCLKoA63LN7ZTF6dWN1"; //PRODTODO
+  // const subgraphUri = "https://api.studio.thegraph.com/query/60402/gospel-v2/version/latest"; //PRODTODO
+  // const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract"; //PRODTODO
+  
+  const httpLink = createHttpLink({
+    uri: subgraphUri,
+  });
+
+// Middleware to add headers dynamically (e.g., from cookies or environment)
+const authLink = setContext(async (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPH_API_KEY}`,
+    }
+  }
+});
+
+// Export a function to create a new client (one per request on the server)
+export function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+}
