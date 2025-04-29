6f555f8595dc988cd4b6930ec0ba4a28077e96c9
nathan-websculpt <websculpt.nathan@gmail.com>
2025-04-29 17:26:17 -0500

    .

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -14,6 +14,7 @@ import { Header } from "~~/components/site-wide/Header";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+import {apollo-client} from "~~/lib/apollo-client";
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   const price = useNativeCurrencyPrice();
