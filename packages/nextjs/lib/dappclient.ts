c4349c1f23334ccf408d85bc62c04add284a5a70
nathan-websculpt <websculpt.nathan@gmail.com>
2025-04-30 10:02:13 -0500

    added a comment  tag before getting absolutely wonky with git

--- packages/nextjs/lib/dappclient.ts
+++ packages/nextjs/lib/dappclient.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
+// TAG: A1
+
 const subgraphUri = "https://gateway.thegraph.com/api/subgraphs/id/FUv8Kpg9nkGhwtALr4YCYJwLDuCLKoA63LN7ZTF6dWN1"; //PRODTODO
 // const subgraphUri = "https://api.studio.thegraph.com/query/60402/gospel-v2/version/latest"; //PRODTODO
 // const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract"; //PRODTODO
